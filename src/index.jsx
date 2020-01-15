af3b4859375758f39dac24eec4557d91c39f5a96
Nami Izarra <nami.izarra@gmail.com>
2020-01-15 16:01:32 +0900

    updating state tree and adding logger and promise

--- src/index.jsx
+++ src/index.jsx
@@ -2,20 +2,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { logger } from 'redux-logger';
+import reduxPromise from 'redux-promise';
 
 // internal modules
 import App from './components/app';
 import '../assets/stylesheets/application.scss';
 
+// reducers
+import messagesListReducer from './reducers/messages_list_reducer';
+import channelsListReducer from './reducers/channels_list_reducer';
+import selectedChannelReducer from './reducers/selected_channel_reducer';
+import currentUserReducer from './reducers/current_user_reducer';
+
+const initialState = {
+  messagesList: [],
+  channelsList: [ "Japan", "Programmers", "Dogs" ],
+  selectedChannel: "Japan",
+  currentUser: prompt("What is your username?") || `anonymous${Math.floor(10 + (Math.random() * 90))}`
+};
+
 // State and reducers
 const reducers = combineReducers({
-  changeMe: (state = null, action) => state
+  messagesList: messagesListReducer,
+  channelsList: channelsListReducer,
+  selectedChannel: selectedChannelReducer,
+  currentUser: currentUserReducer
 });
+
+const middlewares = applyMiddleware(reduxPromise, logger);
 
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={createStore(reducers, initialState, middlewares)}>
     <App />
   </Provider>,
   document.getElementById('root')
